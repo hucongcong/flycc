fd2d5344d3d599536610d3df1942286fe4a834d4
hucongcong <flycc258@163.com>
2022-07-08 23:38:32 +0800

    commander注册基本命令

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -3,14 +3,17 @@ const pkg = require('../package.json')
 const semver = require('semver')
 const colors = require('colors')
 
+const { program } = require('commander')
+
 const log = require('@flycc/log')
 const { MIN_NODE_VERSION } = require('./constants')
+const path = require('path')
 
 /**
  * 检查版本号
  */
 function checkPkgVersion() {
-  log.notice('cli', '当前脚手架版本' + pkg.version)
+  // log.notice('cli', '当前脚手架版本' + pkg.version)
 }
 
 /**
@@ -36,19 +39,37 @@ function checkRoot() {
   rootCheck()
 }
 
+/**
+ * 检查环境变量
+ */
+function checkEnv() {
+  // 获取userHome
+  const userHome = require('os').homedir()
+  // console.log(userHome)
+  const config = require('dotenv').config({
+    path: path.join(userHome, '.flycc')
+  })
+  // console.log('环境变量', config)
+}
+
 function core() {
-  // TODO
   // console.log('core执行了')
   try {
     // 检查入参
-    checkInputArgs()
+    // checkInputArgs()
     // 检查当前脚手架版本
     checkPkgVersion()
     // 检查nodejs版本
     checkNodeVersion()
     // root用户降级处理
     checkRoot()
+    // 检查环境变量
+    checkEnv()
+    // 检查是否有新的版本
+    checkGlobalUpdate()
     // 执行初始化逻辑
+    // 注册命令
+    registerCommand()
     log.success('cli', '初始化成功')
     log.verbose('cli', 'debug模式开启')
   } catch (e) {
@@ -66,6 +87,60 @@ function checkInputArgs() {
   if (args.debug) {
     // 在debug模式下
     log.level = 'verbose'
+  }
+}
+
+/**
+ * 检查是否需要全局更新
+ */
+function checkGlobalUpdate() {
+  // 1. 获取当前的版本号和模块名（从package.json中获取）
+  const currVersion = pkg.version
+  const currName = pkg.name
+  // console.log(currVersion, currName)
+  // 2. 调用npm的api,获取到最新的版本号, 重新写一个包，方便后续复用
+  // 3. 和当前版本进行比对
+}
+
+/**
+ * 注册命令
+ */
+function registerCommand() {
+  program
+    .version(pkg.version)
+    .name(Object.keys(pkg.bin)[0])
+    .description('hcc自定义的脚手架，用于快速初始化项目和一键部署')
+    .usage('<command> [options]')
+    .option('-d --debug', '是否开启调试模式', false)
+
+  // 监听debug事件
+  program.on('option:debug', () => {
+    if (program.debug) {
+      // 在debug模式下
+      log.level = 'verbose'
+    } else {
+      log.level = 'info'
+    }
+  })
+
+  // 处理未知的命令
+  program.on('command:*', (obj) => {
+    // console.log(obj)
+    const availableCommands = program.commands.map((cmd) => {
+      // console.log(cmd)
+      return cmd.name()
+    })
+    console.log(colors.red('未知的命令 ' + obj[0]))
+    if (availableCommands.length > 0) {
+      console.log(colors.green('可用命令' + availableCommands.join(',')))
+    }
+  })
+
+  if (process.argv.length < 3) {
+    // 提供帮助信息
+    program.help()
+  } else {
+    program.parse(process.argv)
   }
 }
 
---
+++ utils/get-npm-info/lib/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function getNpmInfo() {
+  // TODO
+}
+
+module.exports = {
+  getNpmInfo
+}
